f769955f7b88255291d2609ea305758438946c70
HanNayun <HanNayun0.lemon@gmail.com>
2021-08-14 21:45:55 +0800

    31-35 Clear

--- code.js
+++ code.js
@@ -6,11 +6,13 @@ const test = (s) => {
 
 //Input
 
-const par1 = '((((())'
-const par2 = 'BAA'
+const par1 = "()()))"
+const par2 = [[1, 3, 2], [2, 3, 0], [1, 3, 0], [0, 2, 1]]
+const par3 = [[1, 3], [0, 2]]
 
 //Output
 console.log(test(
     par1,
-    par2
+    par2,
+    par3
 ))
